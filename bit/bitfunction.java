fa14c04137aee773ba4a12f904be02c3f0809aca
mehvashkhan <mehvash.khan@somaiya.edu>
2018-01-03 18:25:36 +0530

    Bit manipulation functions

--- bit/bitfunction.java
+++ bit/bitfunction.java
@@ -1,10 +1,13 @@
 package bit;
 
 public class bitfunction{
-	public boolean getbit(int num, int i)
+	public int getbit(int num, int i)
 	{
-		return((num&(1<<i))!=0);
-	}
+		if((num&(1<<i))!=0)
+		return 1;
+	else
+		return 0;
+			}
 
 	public int setbit(int num,int i)
 	{
