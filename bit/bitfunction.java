6bf4ef61f0029b5c0928a06dbe3a3c8e834047f0
mehvashkhan <mehvash.khan@somaiya.edu>
2018-01-03 20:02:33 +0530

    bit manipulation functions

--- bit/bitfunction.java
+++ bit/bitfunction.java
@@ -19,6 +19,17 @@ public class bitfunction{
 	return(num&(~(1<<i)));
 	}
 	
+	public int countsetbits(int n)
+	{
+		int c=0;
+		while(n>0)
+		{
+			n= n&(n-1);
+			c++;
+		}
+		return c;
+	}
+	
 	
 	
 }
